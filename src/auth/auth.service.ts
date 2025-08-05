52816609536d857f04186070e3db084d1b7db11d
RM <marufrasul24@gmail.com>
2025-08-05 13:38:46 +0900

    fix: Создали сущности для логики категорий

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,16 @@
-import { Injectable, BadRequestException } from '@nestjs/common';
+import {
+  Injectable,
+  BadRequestException,
+  UnauthorizedException,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { AuthDto } from './dto/auth.dto';
 import { hash } from 'argon2';
 import { faker } from '@faker-js/faker';
 import { JwtService } from '@nestjs/jwt';
 import { User } from '@prisma/client'; // Добавлен импорт типа User
+import { verify } from 'argon2'; // ✅ Правильно
 
 @Injectable()
 export class AuthService {
@@ -12,6 +18,33 @@ export class AuthService {
     private prisma: PrismaService,
     private jwt: JwtService,
   ) {}
+
+  async login(dto: AuthDto) {
+    const user = await this.validateUser(dto);
+    const token = await this.issueTokens(user.id);
+    return {
+      user: this.returnUserFields(user),
+      ...token,
+    };
+  }
+
+  async getNewTokens(refreshToken: string) {
+    const result = await this.jwt.verifyAsync(refreshToken);
+    if (!result) throw new UnauthorizedException('Invalid refresh token');
+
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: result.id,
+      },
+    });
+    if (!user) throw new NotFoundException('User not found');
+    const tokens = await this.issueTokens(user.id);
+
+    return {
+      user: this.returnUserFields(user),
+      ...tokens,
+    };
+  }
 
   async register(dto: AuthDto) {
     const oldUser = await this.prisma.user.findUnique({
@@ -61,5 +94,19 @@ export class AuthService {
       id: user.id,
       email: user.email,
     };
+  }
+  private async validateUser(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (!user) throw new NotFoundException('User not found');
+
+    const isValid = await verify(user.password, dto.password);
+
+    if (!isValid) throw new UnauthorizedException('Invalid password');
+    return user;
   }
 }
